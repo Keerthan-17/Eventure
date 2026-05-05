5a7bf2a7b98721778b62f20772323e8f45a8ef94
Keerthan M <kituk2004@gmail.com>
2026-05-06 00:18:02 +0530

    update sensitive keys.

--- btrpy/settings.py
+++ btrpy/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-83$pc037y70gf3t75nhi@6l&&-(tm98f8*eseivso%m01%93$4'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -98,8 +98,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'eventure',
-        'USER' : 'root',
-        'PASSWORD' : 'root',
+        'USER' : os.environ.get('DB_USER'),
+        'PASSWORD' : os.environ.get('DB_PASSWORD'),
         'HOST' : 'localhost',
         'PORT' : 3306
     }
@@ -162,6 +162,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'eventur.event123@gmail.com'
-EMAIL_HOST_PASSWORD = 'xrpb ajkm qyzg stlq'
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 DEFAULT_FROM_EMAIL = 'eventur.event123@gmail.com'
